f4222a9b13775fec8bc262edfe57e3960c4e3e75
lmj038 <121388930+lmj038@users.noreply.github.com>
2022-12-25 21:49:35 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,7 @@
+basic.showIcon(IconNames.SmallSquare)
+basic.forever(function () {
+    cbit_小车类.RGB_Car_Big2(cbit_小车类.enColor.White)
+    basic.pause(500)
+    cbit_小车类.RGB_Car_Big2(cbit_小车类.enColor.OFF)
+    basic.pause(500)
+})
